da00a005dd1ce697ecd7cd5e72003ff94c14b3e0
simocompsci <hamids9alo7777@gmail.com>
2026-04-05 23:12:06 +0100

    some recap

--- logTypes.py
+++ logTypes.py
@@ -6,3 +6,8 @@ logging.info('An event happened')
 logging.warning('Something could go wrong')
 logging.error('An error has occurred.')
 logging.critical('The program is unable to recover and will now terminate!')
+
+
+supplies = ['pens', 'staplers', 'flamethrowers', 'binders']
+for index, item in enumerate(supplies):
+    print('Index ' + str(index) + ' in supplies is: ' + item)
