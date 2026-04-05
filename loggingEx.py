c212193c7fa925e90e6ad42cb6bfef35521525f2
simocompsci <hamids9alo7777@gmail.com>
2026-04-05 18:46:06 +0100

    some recap

--- loggingEx.py
+++ loggingEx.py
@@ -1,12 +1,16 @@
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s -')
 logging.debug('Start of program')
-%(levelname)s -
+
+
 def factorial(n):
     logging.debug('Start of factorial(' + str(n) + ')')
     total = 1
-    for i in range(n + 1):
+    for i in range(1 ,n + 1):
         total *= i
-    logging.debug('i is ' + str(i) + ', total is ' + str(total))
+        logging.debug('i is ' + str(i) + ', total is ' + str(total))
     logging.debug('End of factorial(' + str(n) + ')')
-return total
+    return total
+
+print(factorial(5))
+logging.debug('End of program')
