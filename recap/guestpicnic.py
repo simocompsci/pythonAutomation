d7850c2d8fda643d68f25714774431af6d0eb34d
simocompsci <hamids9alo7777@gmail.com>
2026-04-06 16:28:35 +0100

    some recap

---
+++ recap/guestpicnic.py
@@ -0,0 +1,21 @@
+all_guests = {
+    "Alice": {"apples": 5, "pretzels": 12},
+    "Bob": {"ham sandwiches": 3, "apples": 2},
+    "Carol": {"cups": 3, "apple pies": 1},
+}
+
+
+def total_brought(guests, item):
+    num_bought = 0
+    for k, v in guests.items():
+        num_bought = num_bought + v.get(item, 0)
+    return num_bought
+
+
+
+print('Number of things being brought:')
+print(' - Apples' + str(total_brought(all_guests, 'apples')))
+print(' - Cups' + str(total_brought(all_guests, 'cups')))
+print(' - Cakes' + str(total_brought(all_guests, 'cakes')))
+print(' - Ham Sandwiches ' + str(total_brought(all_guests, 'ham sandwiches')))
+print(' - Apple Pies' + str(total_brought(all_guests, 'apple pies')))
