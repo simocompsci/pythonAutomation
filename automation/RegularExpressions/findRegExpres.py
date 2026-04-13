0fffe5aa82329624a587a51889c126e30e9f8f7f
simocompsci <hamids9alo7777@gmail.com>
2026-04-13 17:28:22 +0100

    continuing the automation projects

--- automation/RegularExpressions/findRegExpres.py
+++ automation/RegularExpressions/findRegExpres.py
@@ -9,4 +9,20 @@ mo = phone_re.search('My number is 415-555-4242.')
 print(mo.group(1)) # Returns the first group of the matched text
 print(mo.group(2)) # Returns the second group of the matched text
 print(mo.group(0)) # Returns the full matched text
-mo.group()
+print(mo.groups()) # retrieve all groups at ounce
+
+area_code, main_number = mo.groups()
+print(area_code)
+print(main_number)
+
+pattern = re.compile(r'(\(\d\d\d\)) (\d\d\d-\d\d\d\d)') # using escape characters to escape the parenthesis
+mo = pattern.search('My phone number is (415) 555-4242.')
+print(mo.group(1))
+print(mo.group(2))
+
+# in regular expressions these chars : # $ & ( ) * + - . ? [ \ ] ^ { | } ~
+# have special meaning , that 's why we need to escape them
+
+# If you want to detect these characters as part of your text pattern, you
+# need to escape them with a backslash:
+# \# \$ \& \( \) \* \+ \- \. \? \[ \\ \] \^ \{ \| \} \~
